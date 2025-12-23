15cc23790a7ef4a15081a1d92d427fb2dcf70daa
Damir Usupov <your.Damir412@mail.ru>
2025-12-23 14:29:55 +0300

    "Добавление тестового модуля с LLM"

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Settings:
     # Максимальные и минимальные значения характеристик
     MAX_MANA: int = 100
     MAX_HP: int = 100
     MIN_MANA: int = 0
     MIN_HP: int = 0
+    PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 
 # Создаем экземпляр для импорта
 settings = Settings()
